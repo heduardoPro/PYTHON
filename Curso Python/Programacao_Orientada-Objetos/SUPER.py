3e7db03f355c1c841f3720fb625f7880afaea4f5
Henrique Eduardo <heduardocr7@gmail.com>
2023-05-23 12:09:48 -0300

    Super

---
+++ Curso Python/Programacao_Orientada-Objetos/SUPER.py
@@ -0,0 +1,53 @@
+class MinhaString(str):
+
+    def upper(self):
+        return self.__class__(self.__str__().upper())
+    
+#string = MinhaString('chamando a classe MinhaString')
+#print(string)
+#print(string.upper())
+
+
+
+
+class A(object):
+    atributo_a = 'valor a'
+
+    def __init__(self, atributo):
+        self.atributo = atributo
+
+    def metodo(self):
+        print('A')
+
+
+class B(A):
+    atributo_b = 'valor b'
+
+    def __init__(self, atributo, outra_coisa):
+        super().__init__(atributo)
+        self.outra_coisa = outra_coisa
+
+    def metodo(self):
+        print('B')
+
+
+class C(B):
+    atributo_c = 'valor c'
+
+    def __init__(self, *args, **kwargs):
+        # print('EI, burlei o sistema.')
+        super().__init__(*args, **kwargs)
+
+    def metodo(self):
+        # super().metodo()  # B
+        # super(B, self).metodo()  # A
+        # super(A, self).metodo()  # object
+        A.metodo(self)
+        B.metodo(self)
+        print('C')
+
+c = C('Atributo', 'Qualquer')
+c.metodo()
+print(c.atributo_a)
+print(c.atributo_b)
+print(c.atributo_c)
