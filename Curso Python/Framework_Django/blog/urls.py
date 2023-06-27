2629358560e5231be7b9552fc611c1647cab5760
heduardoPro <heduardocr7@gmail.com>
2023-06-27 17:17:11 -0300

    config urls, views and templates

--- Curso Python/Framework_Django/blog/urls.py
+++ Curso Python/Framework_Django/blog/urls.py
@@ -1,10 +1,13 @@
+
+from blog import views
 from django.urls import path
-from . import views 
 
 app_name = 'blog'
 
-#Blog
+# blog/
+# Django URLs:
+# https://docs.djangoproject.com/en/4.2/topics/http/urls/
 urlpatterns = [
-    path('', views.blog, name='home')
-
+    path('', views.blog, name='home'),
+    path('<int:post_id>/', views.post, name='post'),
 ]
--- Curso Python/Framework_Django/blog/views.py
+++ Curso Python/Framework_Django/blog/views.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+from blog.data import posts
+from django.http import Http404, HttpRequest
 from django.shortcuts import render
 
 
@@ -5,11 +9,36 @@ def blog(request):
     print('blog')
 
     context = {
-        'text': 'Olá blog'
+        # 'text': 'Olá blog',
+        'posts': posts
     }
 
     return render(
         request,
         'blog/index.html',
-        context,
+        context
+    )
+
+
+def post(request: HttpRequest, post_id: int):
+    found_post: dict[str, Any] | None = None
+
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post
+            break
+
+    if found_post is None:
+        raise Http404('Post não existe.')
+
+    context = {
+        # 'text': 'Olá blog',
+        'post': found_post,
+        'title': found_post['title'] + ' - ',
+    }
+
+    return render(
+        request,
+        'blog/post.html',
+        context
     )
